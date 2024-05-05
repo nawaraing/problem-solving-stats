f04586903aaa27ef325518bf82ee1a2708e3d0d9
강준현 <nawaddaing@gmail.com>
2024-05-05 23:10:12 +0900

    [feat] pagination 개발

--- src/problem-solving-stats/crawling.py
+++ src/problem-solving-stats/crawling.py
@@ -38,43 +38,60 @@ class MsgData:
 def crawling():
     msg_datas = []
     msg_data_id = 1
+    soup = BeautifulSoup()
 
     for data in personal_info.list_of_datas:
-        url = 'https://www.acmicpc.net/status?problem_id=&user_id=' + data[personal_info.BAEKJUN_ID] + '&language_id=-1&result_id=-1'
-        print("url: " + url)
-
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-        }
-        response = requests.get(url, headers=headers)
-        # response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        print("soup: " + str(soup))
-
-        # 원하는 요소 추출
-        table = soup.find('table')
-        # print('table: ' + str(table))
-        rows = table.find_all('tr')
-
         solve_problem_number = 0
         number_of_attempt = 0
-        # 첫 번째 행은 헤더이므로 건너뜁니다.
-        for row in rows[1:]:
-            columns = row.find_all('td')
 
-            # 일주일 이내에 제출한 기록인지 확인
-            original_time = columns[SUBMIT_TIME].find('a').get('title')
-            today = datetime.today().date()
-            parsed_date = datetime.strptime(original_time, '%Y-%m-%d %H:%M:%S').date()
-            if (today - parsed_date).days > 7:
-                continue
+        is_done = False # 다음 페이지로 넘어갈 필요가 있는지 판단
+        is_first = True
+        while not is_done:
+            if is_first:
+                url = 'https://www.acmicpc.net/status?problem_id=&user_id=' + data[personal_info.BAEKJUN_ID] + '&language_id=-1&result_id=-1'
+                is_first = False
+            else:
+                # print("soup2: " + str(soup))
+                next_page = soup.find(id='next_page').get('href')
+                # print(next_page)
+                url = 'https://www.acmicpc.net' + next_page
+            print("url: " + url)
 
-            # 시도한 횟수
-            number_of_attempt += 1
-            
-            # 맞은 문제 수
-            if columns[RESULT].text == '맞았습니다!!':
-                solve_problem_number += 1
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+            response = requests.get(url, headers=headers)
+            # response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # print("soup1: " + str(soup))
+
+            # 원하는 요소 추출
+            table = soup.find('table')
+            # print('table: ' + str(table))
+            rows = table.find_all('tr')
+
+            # 첫 번째 행은 헤더이므로 건너뜁니다.
+            for row in rows[1:]:
+                columns = row.find_all('td')
+
+                # 일주일 이내에 제출한 기록인지 확인
+                original_time = columns[SUBMIT_TIME].find('a').get('title')
+                today = datetime.today().date()
+                parsed_date = datetime.strptime(original_time, '%Y-%m-%d %H:%M:%S').date()
+                if (today - parsed_date).days < 1:
+                    continue
+                if (today - parsed_date).days > 7:
+                    is_done = True
+                    break
+
+                # 시도한 횟수
+                number_of_attempt += 1
+                
+                # 맞은 문제 수
+                if columns[RESULT].text == '맞았습니다!!' or '점' in columns[RESULT].text:
+                    # print(columns[RESULT].text)
+                    solve_problem_number += 1
+                    # print("date: " + str(parsed_date) + '  problem_id: ' + str(columns[PROBLEM_ID]))
 
         # solved.ac api
         headers = {
@@ -87,17 +104,17 @@ def crawling():
 
         # 사용자 티어
         member_tier = tier.TIER_ID_TO_STR[int(api_result['tier'])]
-        print(member_tier)
+        # print(member_tier)
 
         # 사용자 래이팅
         member_rating = int(api_result['rating'])
-        print(member_rating)
+        # print(member_rating)
 
         # 사용자 랭킹
         member_rank = int(api_result['rank'])
-        print(member_rank)
+        # print(member_rank)
 
-        # 
+        # msg_data 채우기
         msg_data = MsgData()
         msg_data.member_baekjun_id = data[personal_info.BAEKJUN_ID]
         msg_data.member_email
@@ -116,6 +133,6 @@ def crawling():
         msg_data.member_rating = member_rating
         msg_data.member_rank = member_rank
 
-        # print(msg_data)
+        print(msg_data)
         msg_datas.append(msg_data)
     return msg_datas
