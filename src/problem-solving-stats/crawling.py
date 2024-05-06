0925c624d5b222206cf89445d72b5810068115d9
강준현 <nawaddaing@gmail.com>
2024-05-06 15:50:35 +0900

    [feat] 202 응답 시에 재요청

--- src/problem-solving-stats/crawling.py
+++ src/problem-solving-stats/crawling.py
@@ -60,9 +60,13 @@ def crawling():
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
             }
-            response = requests.get(url, headers=headers)
-            # response = requests.get(url)
-            print("response.status_code: " + str(response.status_code))
+            for i in range(20):
+                response = requests.get(url, headers=headers)
+                # response = requests.get(url)
+                print("response.status_code: " + str(response.status_code))
+                if (response.status_code == 200):
+                    break
+                print("re request!!")
             print("response.text: " + response.text)
             soup = BeautifulSoup(response.text, 'html.parser')
             # print("soup: " + str(soup))
